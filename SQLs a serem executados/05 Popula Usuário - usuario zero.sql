85b47193028622e09e209cadf62eff191f0242a4
Ygor santos <ygorats@gmail.com>
2016-12-05 01:15:45 -0200

    Atualizações gerais.

--- SQLs a serem executados/05 Popula Usuário - usuario zero.sql
+++
@@ -1,2 +0,0 @@
-
-INSERT INTO USUARIO(ID, CODIGO) VALUES(cast(cast(0 as binary) as uniqueidentifier), '10000000')
---
+++ SQLs a serem executados/07 Popula Usuário - usuario zero.sql
@@ -0,0 +1,4 @@
+ 
+
+INSERT INTO USUARIO(ID, EMAILINSTITUCIONAL, ATIVO) VALUES(cast(cast(0 as binary) as uniqueidentifier), 'alfa@inf.ufg.br', 1);
+
